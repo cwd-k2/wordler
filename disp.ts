ba4bda15800c5c621df2a90dd9b3678b0e06cc8d
sotalbireo <athene609.cyl@gmail.com>
2022-02-08 21:37:02 +0900

    feature; display input history like original Wordle

--- disp.ts
+++ disp.ts
@@ -1,4 +1,12 @@
+import {
+  bgBrightGreen,
+  bgBrightYellow,
+  bgWhite,
+  black,
+  bold,
+} from "https://deno.land/std@0.125.0/fmt/colors.ts";
 import { availableKeys, CharHit } from "./game.ts";
+import type { WordleGame } from "./game.ts";
 
 function colorize(c: CharHit): string {
   switch (c) {
@@ -13,14 +21,23 @@ function colorize(c: CharHit): string {
   }
 }
 
-export function display(s: {
-  readonly guesses: number;
-  readonly hist: CharHit[][];
-  readonly chrs: Map<string, CharHit>;
-}): string {
+function colorizeWithEscape([s, c]: [string, CharHit]): string {
+  s = ` ${s} `
+  switch (c) {
+    case "not yet":
+    case "not included":
+      return bgWhite(black(s));
+    case "included":
+      return bgBrightYellow(black(bold(s)));
+    case "exact":
+      return bgBrightGreen(black(bold(s)));
+  }
+}
+
+export function display(s: ReturnType<WordleGame["status"]>): string {
   let res = "";
   res += `guesses: ${s.guesses}\n`;
-  res += s.hist.map((l) => l.map(colorize).join("")).join("\n");
+  res += s.hist.map((l) => l.map(colorizeWithEscape).join("")).join("\n");
   res += "\n";
   availableKeys.forEach((k) => {
     res += `(${k}: ${colorize(s.chrs.get(k) || "not yet")})`;
